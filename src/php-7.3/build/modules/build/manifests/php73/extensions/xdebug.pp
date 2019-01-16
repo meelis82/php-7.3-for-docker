b0026e48807516a1892f0333847e491aeaf259f6
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 20:50:15 +0200

    .

--- src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
+++ src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
@@ -21,11 +21,11 @@ class build::php73::extensions::xdebug {
 
   bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1']
   }
 
-  bash_exec { 'cd /tmp/2.7.0beta1 && make install':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && make install':
     timeout => 0,
-    require => Bash_exec['cd /tmp/2.7.0beta1 && make']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && make']
   }
 }
