203ed050832fda461e697e9e123704d009031988
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 16:00:22 +0200

    xdebug update.

--- src/php-7.1/build/modules/build/manifests/php71/extensions/xdebug.pp
+++ src/php-7.1/build/modules/build/manifests/php71/extensions/xdebug.pp
@@ -1,31 +1,31 @@
 class build::php71::extensions::xdebug {
   require build::php71
 
-  file { '/tmp/xdebug-2.4.0.tgz':
+  file { '/tmp/xdebug-2.6.1.tgz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/xdebug-2.4.0.tgz'
+    source => 'puppet:///modules/build/tmp/xdebug-2.6.1.tgz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf xdebug-2.4.0.tgz':
-    require => File['/tmp/xdebug-2.4.0.tgz']
+  bash_exec { 'cd /tmp && tar xzf xdebug-2.6.1.tgz':
+    require => File['/tmp/xdebug-2.6.1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.4.0 && phpize-7.1.26':
-    require => Bash_exec['cd /tmp && tar xzf xdebug-2.4.0.tgz']
+  bash_exec { 'cd /tmp/xdebug-2.6.1 && phpize-7.1.26':
+    require => Bash_exec['cd /tmp && tar xzf xdebug-2.6.1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.4.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.1.26':
+  bash_exec { 'cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.1.26':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.4.0 && phpize-7.1.26']
+    require => Bash_exec['cd /tmp/xdebug-2.6.1 && phpize-7.1.26']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.4.0 && make':
+  bash_exec { 'cd /tmp/xdebug-2.6.1 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.4.0 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.1.26']
+    require => Bash_exec['cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.1.26']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.4.0 && make install':
+  bash_exec { 'cd /tmp/xdebug-2.6.1 && make install':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.4.0 && make']
+    require => Bash_exec['cd /tmp/xdebug-2.6.1 && make']
   }
 }
