428ced8568616184276f193c80e56ef2437dcf58
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 21:59:20 +0200

    cleanup

--- src/php-7.3/clean/modules/clean/manifests/init.pp
+++ src/php-7.3/clean/modules/clean/manifests/init.pp
@@ -1,5 +1,14 @@
 class clean {
-  bash_exec { 'apt-get clean': }
+
+  bash_exec { 'apt-get remove -y build-essential autoconf autocmake': }
+
+  bash_exec { 'apt-get autoremove':
+    'require' => bash_exec[ 'apt-get remove -y build-essential autoconf autocmake' ]
+  }
+
+  bash_exec { 'apt-get clean':
+    'require' => bash_exec[ 'apt-get autoremove' ]
+  }
 
   bash_exec { 'rm -rf /tmp/*': }
 
