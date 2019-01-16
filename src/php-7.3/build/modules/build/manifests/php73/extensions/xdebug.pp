5a9ed95f7b640277bf9f2651779801aa1a2b76e3
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 20:48:26 +0200

    xdebug

--- src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
+++ src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
@@ -1,31 +1,31 @@
 class build::php73::extensions::xdebug {
   require build::php73
 
-  file { '/tmp/xdebug-2.6.1.tgz':
+  file { '/tmp/xdebug-2.7.0beta1.tgz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/xdebug-2.6.1.tgz'
+    source => 'puppet:///modules/build/tmp/xdebug-2.7.0beta1.tgz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf xdebug-2.6.1.tgz':
-    require => File['/tmp/xdebug-2.6.1.tgz']
+  bash_exec { 'cd /tmp && tar xzf xdebug-2.7.0beta1.tgz':
+    require => File['/tmp/xdebug-2.7.0beta1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && phpize-7.3.1':
-    require => Bash_exec['cd /tmp && tar xzf xdebug-2.6.1.tgz']
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && phpize-7.3.1':
+    require => Bash_exec['cd /tmp && tar xzf xdebug-2.7.0beta1.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && phpize-7.3.1']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0beta1 && phpize-7.3.1']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && make':
+  bash_exec { 'cd /tmp/xdebug-2.7.0beta1 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1']
+    require => Bash_exec['cd /tmp/2.7.0beta1 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.1']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.6.1 && make install':
+  bash_exec { 'cd /tmp/2.7.0beta1 && make install':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.6.1 && make']
+    require => Bash_exec['cd /tmp/2.7.0beta1 && make']
   }
 }
