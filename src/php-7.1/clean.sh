c6d6ebf948d7897bbacacded652ebb7fe66fdf08
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 14:38:03 +0200

    Typo fix.

--- src/php-7.1/clean.sh
+++ src/php-7.1/clean.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-puppet apply --modulepath=/src/php-7.0/clean/modules /src/php-7.0/clean/clean.pp
+puppet apply --modulepath=/src/php-7.1/clean/modules /src/php-7.1/clean/clean.pp
