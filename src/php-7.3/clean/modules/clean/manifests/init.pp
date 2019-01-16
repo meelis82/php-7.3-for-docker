9d0c9a2086b6f6787cc58b500d6d692fa98e675e
Meelis Valgeväli <meelis@adm.ee>
2019-01-16 23:36:03 +0200

    cleanup

--- src/php-7.3/clean/modules/clean/manifests/init.pp
+++ src/php-7.3/clean/modules/clean/manifests/init.pp
@@ -1,6 +1,10 @@
 class clean {
 
-  bash_exec { 'apt-get remove -y build-essential autoconf automake': }
+  bash_exec { 'apt-get remove -y $(dpkg -l|grep '\-dev'|awk '{ print $2 }')': }
+
+  bash_exec { 'apt-get remove -y build-essential autoconf automake':
+    require => Bash_exec[ 'apt-get remove -y $(dpkg -l|grep '\-dev'|awk '{ print $2 }')' ]
+  }
 
   bash_exec { 'apt-get autoremove -y':
     require => Bash_exec[ 'apt-get remove -y build-essential autoconf automake' ]
