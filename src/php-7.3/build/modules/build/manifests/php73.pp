48b5675274377591ed5fae4ca01414f43b5680f3
Meelis Valgeväli <meelis@adm.ee>
2019-07-04 14:02:58 +0300

    7.3.2 -> 7.3.7

--- src/php-7.3/build/modules/build/manifests/php73.pp
+++ src/php-7.3/build/modules/build/manifests/php73.pp
@@ -3,49 +3,49 @@ class build::php73 {
   require build::php73::supervisor
   require build::phpfarm
 
-  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.2/etc/conf.d': }
+  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.7/etc/conf.d': }
 
-  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.2/etc/fpm.d': }
+  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.7/etc/fpm.d': }
 
-  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.2/etc/pool.d': }
+  bash_exec { 'mkdir -p /usr/local/src/phpfarm/inst/php-7.3.7/etc/pool.d': }
 
-  file { '/tmp/php-7.3.2.tar.gz':
+  file { '/tmp/php-7.3.7.tar.gz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/php-7.3.2.tar.gz'
+    source => 'puppet:///modules/build/tmp/php-7.3.7.tar.gz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf php-7.3.2.tar.gz':
-    require => File['/tmp/php-7.3.2.tar.gz']
+  bash_exec { 'cd /tmp && tar xzf php-7.3.7.tar.gz':
+    require => File['/tmp/php-7.3.7.tar.gz']
   }
 
-  bash_exec { 'mv /tmp/php-7.3.2 /usr/local/src/phpfarm/src/php-7.3.2':
-    require => Bash_exec['cd /tmp && tar xzf php-7.3.2.tar.gz']
+  bash_exec { 'mv /tmp/php-7.3.7 /usr/local/src/phpfarm/src/php-7.3.7':
+    require => Bash_exec['cd /tmp && tar xzf php-7.3.7.tar.gz']
   }
 
-  file { '/usr/local/src/phpfarm/src/custom/options-7.3.2.sh':
+  file { '/usr/local/src/phpfarm/src/custom/options-7.3.7.sh':
     ensure => present,
-    source => 'puppet:///modules/build/usr/local/src/phpfarm/src/custom/options-7.3.2.sh',
+    source => 'puppet:///modules/build/usr/local/src/phpfarm/src/custom/options-7.3.7.sh',
     mode => "755",
-    require => Bash_exec['mv /tmp/php-7.3.2 /usr/local/src/phpfarm/src/php-7.3.2']
+    require => Bash_exec['mv /tmp/php-7.3.7 /usr/local/src/phpfarm/src/php-7.3.7']
   }
 
-  bash_exec { '/usr/local/src/phpfarm/src/main.sh 7.3.2':
+  bash_exec { '/usr/local/src/phpfarm/src/main.sh 7.3.7':
     timeout => 0,
-    require => File['/usr/local/src/phpfarm/src/custom/options-7.3.2.sh']
+    require => File['/usr/local/src/phpfarm/src/custom/options-7.3.7.sh']
   }
 
-  bash_exec { 'rm -rf /usr/local/src/phpfarm/src/php-7.3.2':
-    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.2']
+  bash_exec { 'rm -rf /usr/local/src/phpfarm/src/php-7.3.7':
+    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.7']
   }
 
-  file { '/usr/local/src/phpfarm/inst/php-7.3.2/etc/php-fpm.conf':
+  file { '/usr/local/src/phpfarm/inst/php-7.3.7/etc/php-fpm.conf':
     ensure => present,
-    source => 'puppet:///modules/build/usr/local/src/phpfarm/inst/php-7.3.2/etc/php-fpm.conf',
+    source => 'puppet:///modules/build/usr/local/src/phpfarm/inst/php-7.3.7/etc/php-fpm.conf',
     mode => "644",
-    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.2']
+    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.7']
   }
 
-  bash_exec { 'switch-phpfarm 7.3.2':
-    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.2']
+  bash_exec { 'switch-phpfarm 7.3.7':
+    require => Bash_exec['/usr/local/src/phpfarm/src/main.sh 7.3.7']
   }
 }
--- src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
+++ src/php-7.3/build/modules/build/manifests/php73/extensions/xdebug.pp
@@ -10,18 +10,18 @@ class build::php73::extensions::xdebug {
     require => File['/tmp/xdebug-2.7.0RC2.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && phpize-7.3.2':
+  bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && phpize-7.3.7':
     require => Bash_exec['cd /tmp && tar xzf xdebug-2.7.0RC2.tgz']
   }
 
-  bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.2':
+  bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.7':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.7.0RC2 && phpize-7.3.2']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0RC2 && phpize-7.3.7']
   }
 
   bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && make':
     timeout => 0,
-    require => Bash_exec['cd /tmp/xdebug-2.7.0RC2 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.2']
+    require => Bash_exec['cd /tmp/xdebug-2.7.0RC2 && ./configure --with-php-config=/usr/local/src/phpfarm/inst/bin/php-config-7.3.7']
   }
 
   bash_exec { 'cd /tmp/xdebug-2.7.0RC2 && make install':
