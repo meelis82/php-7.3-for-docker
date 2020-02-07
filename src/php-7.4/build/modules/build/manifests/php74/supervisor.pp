be26329db9b4015905ba24db3cce499746e3734a
Meelis Valgeväli <meelis@adm.ee>
2020-02-07 15:13:22 +0200

    supervisor.pp

--- src/php-7.4/build/modules/build/manifests/php74/supervisor.pp
+++ src/php-7.4/build/modules/build/manifests/php74/supervisor.pp
@@ -5,12 +5,10 @@ class build::php74::supervisor {
     mode => "644"
   }
 
-  class build::supervisor {
-    file { '/etc/supervisor/conf.d/supervisord.conf':
-      ensure => present,
-      source => 'puppet:///modules/build/etc/supervisor/conf.d/supervisord.conf',
-      mode => "644"
-    }
+  file { '/etc/supervisor/conf.d/supervisord.conf':
+    ensure => present,
+    source => 'puppet:///modules/build/etc/supervisor/conf.d/supervisord.conf',
+    mode => "644"
   }
 
 }
